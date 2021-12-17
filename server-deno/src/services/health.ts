82f49ae07d5f8ad97f6fb5c13f70ea8c354eda37
Damodar Lohani <dlohani48@gmail.com>
2021-12-17 12:23:37 +0545

    updated twitter link

---
+++ server-deno/src/services/health.ts
@@ -0,0 +1,210 @@
+import { Service } from '../service.ts';
+import { Payload } from '../client.ts';
+import { AppwriteException } from '../exception.ts';
+import type { Models } from '../models.d.ts'
+
+export class Health extends Service {
+    /**
+     * Get HTTP
+     *
+     * Check the Appwrite HTTP server is up and responsive.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async get(): Promise<Response> {
+        let path = '/health';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get Anti virus
+     *
+     * Check the Appwrite Anti Virus server is up and connection is successful.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getAntiVirus(): Promise<Response> {
+        let path = '/health/anti-virus';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get Cache
+     *
+     * Check the Appwrite in-memory cache server is up and connection is
+     * successful.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getCache(): Promise<Response> {
+        let path = '/health/cache';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get DB
+     *
+     * Check the Appwrite database server is up and connection is successful.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getDB(): Promise<Response> {
+        let path = '/health/db';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get Certificate Queue
+     *
+     * Get the number of certificates that are waiting to be issued against
+     * [Letsencrypt](https://letsencrypt.org/) in the Appwrite internal queue
+     * server.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getQueueCertificates(): Promise<Response> {
+        let path = '/health/queue/certificates';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get Functions Queue
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getQueueFunctions(): Promise<Response> {
+        let path = '/health/queue/functions';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get Logs Queue
+     *
+     * Get the number of logs that are waiting to be processed in the Appwrite
+     * internal queue server.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getQueueLogs(): Promise<Response> {
+        let path = '/health/queue/logs';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get Tasks Queue
+     *
+     * Get the number of tasks that are waiting to be processed in the Appwrite
+     * internal queue server.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getQueueTasks(): Promise<Response> {
+        let path = '/health/queue/tasks';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get Usage Queue
+     *
+     * Get the number of usage stats that are waiting to be processed in the
+     * Appwrite internal queue server.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getQueueUsage(): Promise<Response> {
+        let path = '/health/queue/usage';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get Webhooks Queue
+     *
+     * Get the number of webhooks that are waiting to be processed in the Appwrite
+     * internal queue server.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getQueueWebhooks(): Promise<Response> {
+        let path = '/health/queue/webhooks';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get Local Storage
+     *
+     * Check the Appwrite local storage device is up and connection is successful.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getStorageLocal(): Promise<Response> {
+        let path = '/health/storage/local';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+    /**
+     * Get Time
+     *
+     * Check the Appwrite server time is synced with Google remote NTP server. We
+     * use this technology to smoothly handle leap seconds with no disruptive
+     * events. The [Network Time
+     * Protocol](https://en.wikipedia.org/wiki/Network_Time_Protocol) (NTP) is
+     * used by hundreds of millions of computers and devices to synchronize their
+     * clocks over the Internet. If your computer sets its own clock, it likely
+     * uses NTP.
+     *
+     * @throws {AppwriteException}
+     * @returns {Promise}
+     */
+    async getTime(): Promise<Response> {
+        let path = '/health/time';
+        let payload: Payload = {};
+
+        return await this.client.call('get', path, {
+            'content-type': 'application/json',
+        }, payload);
+    }
+}
